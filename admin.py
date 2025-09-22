de9b6be01a9adc70a942ee4d80fcf9c8c4af59a1
Treasure Muriithi <treasure.k.muriithi@gmail.com>
2025-09-22 13:12:53 +0100

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,7 +2,7 @@ from database import fetch_all
 import time
 
 class Admin:
-    def __init__(self, username="admin", password="admin123"):
+    def __init__(self, username="admin", password="PASSWORD"):
         self.__username = username
         self.__password = password
 
